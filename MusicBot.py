481627025a192712a47fd4b6bf24a46b9435aada
TheGregster1111 <79836485+TheGregster1111@users.noreply.github.com>
2022-08-29 12:25:44 +0200

    Update MusicBot.py

--- MusicBot.py
+++ MusicBot.py
@@ -547,7 +547,7 @@ class MainCog(commands.Cog):
                 Button(
                     label = "Website",
                     style = ButtonStyle.URL,
-                    url = 'http://www.trim-keep-354608.ew.r.appspot.com/'
+                    url = 'https://trim-keep-354608.ew.r.appspot.com/'
                 )
             ]
             ]
